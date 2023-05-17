bb53b5f630759550e4696e14f54e91a35429085b
Whxismou1 <melouc00@estudianttes.unileon.es>
2023-05-17 19:29:13 +0200

    Recursos estatico (video 32)

--- Spring_MVC_Ejemplo1/src/main/java/es/whxismou/spring/mvc/HolaAlumnosControlador.java
+++ Spring_MVC_Ejemplo1/src/main/java/es/whxismou/spring/mvc/HolaAlumnosControlador.java
@@ -1,7 +1,11 @@
 package es.whxismou.spring.mvc;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HolaAlumnosControlador {
@@ -17,7 +21,19 @@ public class HolaAlumnosControlador {
 	public String procesarFormulario() {
 		return "HolaAlumnosSpring";
 	}
-	
-	
-	
+
+	@RequestMapping("/procesarFormulario2")
+	//public String otroProcesoFormulario(HttpServletRequest request, Model modelo) {
+	public String otroProcesoFormulario(@RequestParam("nombreAlumno") String nombre, Model modelo) {
+		//String nombre = request.getParameter("nombreAlumno");
+		nombre += " es el mejor alumno";
+
+		String mensajeFinal = "¿Quien es el mejor alumno?" + nombre;
+		
+		//Agregando info al modelo
+		modelo.addAttribute("mensajeClaro", mensajeFinal);
+
+		return "HolaAlumnosSpring";
+	}
+
 }
--- Spring_MVC_Ejemplo1/src/main/webapp/WEB-INF/vista/HolaAlumnosSpring.jsp
+++ Spring_MVC_Ejemplo1/src/main/webapp/WEB-INF/vista/HolaAlumnosSpring.jsp
@@ -1,12 +1,23 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
-    pageEncoding="ISO-8859-1"%>
+	pageEncoding="ISO-8859-1"%>
 <!DOCTYPE html>
 <html>
 <head>
+<link rel="stylesheet" type="text/css" href="${pageContext.request.contextPath}/recursos/estilos/myestilo.css">
 <meta charset="ISO-8859-1">
 <title>Insert title here</title>
 </head>
 <body>
-Hola ${param.nombreAlumno }. Bienvenido al curso de Spring
+	Hola ${param.nombreAlumno }. Bienvenido al curso de Spring
+
+	<p>
+		<br>
+	<h2>Atencion a todos</h2>
+
+	${mensajeClaro}
+
+	</p>
+	<img alt="foto"
+		src="${pageContext.request.contextPath}/recursos/imgs/aa.jpg">
 </body>
 </html>
